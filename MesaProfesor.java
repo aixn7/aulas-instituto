6ade0cd9864d5052e380c6c7e10d3b07e71f13e5
Ivar Fernandez <ivarfermar7@gmail.com>
2021-11-17 09:59:10 +0100

    01 Constructor y atributos clase MesaProfesor

---
+++ MesaProfesor.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Mesa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MesaProfesor
+{
+    private int altura;
+    private String color;
+
+        public MesaProfesor(int alturaMesa, String colorMesa)
+    {
+        altura = alturaMesa;
+        color = colorMesa;
+    }
+
+}
